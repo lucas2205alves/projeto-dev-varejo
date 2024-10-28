39d127d2b97952a6008a2b0a9f335308bdef3580
Lucas Ricardo Domingos Alves <lucas.alves2205@hotmail.com>
2024-10-28 20:09:43 -0300

    Restart do projeto

---
+++ src/test/java/com/lucasalves/projeto_dev_varejo/ProjetoDevVarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasalves.projeto_dev_varejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDevVarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
